ca064b74391445f78b66cb71f6dfa4ffc906503c
hehe <34378901@qq.com>
2023-08-02 14:26:40 +0800

    Minecraft for C update
    Synchronized with Minecraft for C "Created thread lib"
    (commit 47c778).
    Added night mode.

---
+++ js/details.js
@@ -0,0 +1,15 @@
+function details_flip(flip_id) {
+	var item = document.getElementById(flip_id);
+	console.log(item);
+	if (item)
+	{
+		if (item.style.display == "none" || item.style.display == "")
+		{
+			item.style.display = "inline";
+		}
+		else
+		{
+			item.style.display = "none";
+		};
+	};
+};
